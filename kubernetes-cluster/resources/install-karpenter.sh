9f4e599603dd1fb832f2999490daee550de1cddc
arivappa <4rivappa@proton.me>
2026-04-05 13:16:17 +0530

    feat: refactor kubernetes resources to install environment

---
+++ kubernetes-cluster/resources/install-karpenter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+KARPENTER_NAMESPACE=karpenter
+CLUSTER_NAME=socket-tuner
+
+AWS_PARTITION="aws"
+AWS_REGION="us-east-1"
+OIDC_ENDPOINT="$(aws eks describe-cluster --name "${CLUSTER_NAME}" \
+    --query "cluster.identity.oidc.issuer" --output text)"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' \
+    --output text)
+
+export KARPENTER_VERSION="1.10.0"
+
+helm template karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" \
+    --set "settings.clusterName=${CLUSTER_NAME}" \
+    --set "settings.interruptionQueue=${CLUSTER_NAME}" \
+    --set "serviceAccount.annotations.eks\.amazonaws\.com/role-arn=arn:${AWS_PARTITION}:iam::${AWS_ACCOUNT_ID}:role/KarpenterControllerRole-${CLUSTER_NAME}" \
+    --set controller.resources.requests.cpu=1 \
+    --set controller.resources.requests.memory=1Gi \
+    --set controller.resources.limits.cpu=1 \
+    --set controller.resources.limits.memory=1Gi > karpenter.yaml
+
+sed -i 's/\${NODEGROUP}/default-system-nodes/g' karpenter.yaml
+
+kubectl create namespace "${KARPENTER_NAMESPACE}" || true
+kubectl create -f \
+    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v${KARPENTER_VERSION}/pkg/apis/crds/karpenter.sh_nodepools.yaml"
+kubectl create -f \
+    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v${KARPENTER_VERSION}/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml"
+kubectl create -f \
+    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v${KARPENTER_VERSION}/pkg/apis/crds/karpenter.sh_nodeclaims.yaml"
+kubectl apply -f karpenter.yaml
